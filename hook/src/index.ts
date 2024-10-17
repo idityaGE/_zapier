e9613d2fd009899dc6bc9cbc0b42cdfd82e5a614
idityaGE <am44910606@gmail.com>
2024-10-18 01:57:14 +0530

    fuck folder

--- hook/src/index.ts
+++ hook/src/index.ts
@@ -1,7 +1,37 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  console.log("reaqched here");
+  // store in db a new trigger
+  await client.$transaction(async tx => {
+    console.log("reaqched here 2");
+    const run = await client.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body
+      }
+    });
+    console.log("reaqched here 3");
+
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id
+      }
+    })
+  })
+  res.json({
+    message: "Webhook received"
+  })
+})
+
+app.listen(3000);
