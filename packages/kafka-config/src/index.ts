196f0aa3ceee8dacc88725bdf280a326787a88e4
idityaGE <am44910606@gmail.com>
2025-01-01 22:21:28 +0530

    Set default Kafka broker to "localhost:9092" if not specified in environment variables

--- packages/kafka-config/src/index.ts
+++ packages/kafka-config/src/index.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 const TOPIC_NAME = "zap-events";
 
-const KAFKA_BROKER = process.env.KAFKA_BROKER as string;
+const KAFKA_BROKER = process.env.KAFKA_BROKER || "localhost:9092";
 
 export { TOPIC_NAME, KAFKA_BROKER };
